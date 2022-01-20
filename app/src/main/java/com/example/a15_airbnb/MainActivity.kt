2ce492499b9d1b44851033257d744658a543b4aa
Songmi <ksongmi0727@gmail.com>
2022-01-20 23:27:00 +0900

    0120

--- app/src/main/java/com/example/a15_airbnb/MainActivity.kt
+++ app/src/main/java/com/example/a15_airbnb/MainActivity.kt
@@ -3,25 +3,31 @@ package com.example.a15_airbnb
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.naver.maps.geometry.LatLng
 import com.naver.maps.map.*
 import com.naver.maps.map.overlay.Marker
 import com.naver.maps.map.util.FusedLocationSource
 import com.naver.maps.map.util.MarkerIcons
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var naverMap: NaverMap
-    private lateinit var  locationSource : FusedLocationSource
+    private lateinit var locationSource: FusedLocationSource
 
     private val mapView: MapView by lazy {
         findViewById(R.id.mapView)
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mapView.onCreate(savedInstanceState)
-
         mapView.getMapAsync(this)
 
 
@@ -29,36 +35,70 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onMapReady(map: NaverMap) {
         naverMap = map
-
         naverMap.maxZoom = 18.0
         naverMap.minZoom = 10.0
 
         val cameraUpdate = CameraUpdate.scrollTo(LatLng(37.49816261442695, 127.02759795030872))
-
         naverMap.moveCamera(cameraUpdate)
 
         val uiSetting = naverMap.uiSettings
         uiSetting.isLocationButtonEnabled = true
 
-        val locationSource = FusedLocationSource(this@MainActivity, LOCATION_PERMISSION_REQUEST_CODE)
+        val locationSource =
+            FusedLocationSource(this@MainActivity, LOCATION_PERMISSION_REQUEST_CODE)
         naverMap.locationSource = locationSource
 
-        val marker = Marker()
-        marker.position = LatLng(37.500373608735984, 127.02993118417238)
-        marker.map = naverMap
-        marker.icon = MarkerIcons.BLACK
-        marker.iconTintColor = Color.RED
-
+        getHouseListFromAPI()
     }
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+    private fun getHouseListFromAPI() {
+        val retrofit = Retrofit.Builder()
+            .baseUrl("https://run.mocky.io")
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(HouseService::class.java).also {
+            it.getHouseList()
+                .enqueue(object : Callback<HouseDto> {
+                    override fun onResponse(call: Call<HouseDto>, response: Response<HouseDto>) {
+                        if (response.isSuccessful.not()) {
+                            return
+                        }
+                        response.body()?.let { dto ->
+                            updateMarker(dto.items)
+                        }
+                    }
+                    override fun onFailure(call: Call<HouseDto>, t: Throwable) {
+
+                    }
+
+                })
+        }
+    }
+
+    private fun updateMarker(houses: List<HouseModel>){
+        houses.forEach { house ->
+            val marker = Marker()
+            marker.position = LatLng(house.lat, house.lng)
+            marker.map = naverMap
+            marker.tag = house.id
+            marker.icon = MarkerIcons.BLACK
+            marker.iconTintColor = Color.RED
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
 
-        if(requestCode != LOCATION_PERMISSION_REQUEST_CODE){
+        if (requestCode != LOCATION_PERMISSION_REQUEST_CODE) {
             return
         }
-        if(locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)){
-            if (!locationSource.isActivated){
+        if (locationSource.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated) {
                 naverMap.locationTrackingMode = LocationTrackingMode.None
             }
             return
@@ -100,7 +140,7 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mapView.onLowMemory()
     }
 
-    companion object{
+    companion object {
         private const val LOCATION_PERMISSION_REQUEST_CODE = 1000
     }
 }
